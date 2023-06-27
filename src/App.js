be3e0cd6d67b5a34d7bd76416bcbf48988b20bf6
LSYbuilds <builddav2009@gmail.com>
2023-06-27 10:06:24 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import Guide from "./pages/Guide";
 import Calendar from "./pages/Calendar";
 import DayList from "./pages/DayList";
 import Write from "./pages/Write";
-import { Route, Routes } from "react-router";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/intro" element={<Intro/>}/>
-      <Route path="/guide" element={<Guide/>}/>
-      <Route path="/calendar" element={<Calendar/>}/>
-      <Route path="/daylist" element={<DayList/>}/>
-      <Route path="/write" element={<Write/>}/>
-    </Routes>
-
-  )
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route path="/guide" element={<Guide />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/daylist" element={<DayList />} />
+        <Route path="/write" element={<Write />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/DayList.js
+++ src/pages/DayList.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const DayList = () => {
-  return (
-    <div>DayList</div>
-  )
-}
+  return <div>DayListdjoaiwd</div>;
+};
 
-export default DayList
+export default DayList;
